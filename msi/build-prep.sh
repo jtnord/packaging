38d18223f0e2942174cdd8d3e969a8b046664dc5
James Nord <jnord+github@cloudbees.com>
2015-09-02 15:40:09 +0100

    use correct temp directory

--- msi/build-prep.sh
+++ msi/build-prep.sh
@@ -18,12 +18,12 @@ cp -t $D/ $bin/FindJava.java $bin/build.bat $bin/jenkins.exe.config $bin/bootstr
 # perform transalations of the files
 mkdir $D/tmp
 
-unzip -p "$WAR" 'WEB-INF/lib/jenkins-core-*.jar' > tmp/core.jar
-unzip -p tmp/core.jar windows-service/jenkins.exe > tmp/jenkins.exe
-unzip -p tmp/core.jar windows-service/jenkins.xml | sed -e "s|\bjenkins\b|${ARTIFACTNAME}|" | sed -e "s|8080|${PORT}|" > tmp/jenkins.xm_
+unzip -p "$WAR" 'WEB-INF/lib/jenkins-core-*.jar' > $D/tmp/core.jar
+unzip -p $D/tmp/core.jar windows-service/jenkins.exe > $D/tmp/jenkins.exe
+unzip -p $D/tmp/core.jar windows-service/jenkins.xml | sed -e "s|\bjenkins\b|${ARTIFACTNAME}|" | sed -e "s|8080|${PORT}|" > $D/tmp/jenkins.xm_
 # replace executable name to the bundled JRE
-sed -e 's|executable.*|executable>%BASE%\\jre\\bin\\java</executable>|' < tmp/jenkins.xm_ > tmp/jenkins.xml
-rm tmp/core tmp/jenkins.xm_ 
+sed -e 's|executable.*|executable>%BASE%\\jre\\bin\\java</executable>|' < $D/tmp/jenkins.xm_ > $D/tmp/jenkins.xml
+rm $D/tmp/core $D/tmp/jenkins.xm_ 
 
 cp ${PKCS12_FILE} $D/key.pkcs12
 cp ${PKCS12_PASSWORD_FILE} $D/key.password
