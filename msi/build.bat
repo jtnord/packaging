8fd49ad564ce0334b9103a12de454b7561b3f3bc
James Nord <jnord+github@cloudbees.com>
2015-09-02 17:29:11 +0100

    exit on command failure

--- msi/build.bat
+++ msi/build.bat
@@ -16,22 +16,24 @@ exit /B 1
 :OK
 
 REM capture JRE
-javac FindJava.java
-set JREDIR=$(java -cp . FindJava)
-echo "JRE=$JREDIR"
-heat dir "$JREDIR" -o jre.wxs -sfrag -sreg -nologo -srd -gg -cg JreComponents -dr JreDir -var var.JreDir
+%JAVA_HOME%\bin\javac FindJava.java || exit /b 1
+REM Set by Jenkins or tooling to point to the JDK
+set JREDIR=%JAVA_HOME%\jre
+echo "JRE=%JREDIR%"
+REM this seems a little strange - using the JDK configured here to re-package...
+heat dir "%JREDIR%" -o jre.wxs -sfrag -sreg -nologo -srd -gg -cg JreComponents -dr JreDir -var var.JreDir  || exit /b 1
 
 REM  pick up java.exe File ID
-JavaExeId=$(grep java.exe jre.wxs | grep -o "fil[0-9A-F]*")
+set JavaExeId=$(grep java.exe jre.wxs | grep -o "fil[0-9A-F]*")
 
-candle -dJreDir="%JREDIR%" -dWAR="%war%" -dJavaExeId=%JavaExeId% -nologo -ext WixUIExtension -ext WixUtilExtension -ext WixFirewallExtension jenkins.wxs jre.wxs
+candle -dJreDir="%JREDIR%" -dWAR="%war%" -dJavaExeId=%JavaExeId% -nologo -ext WixUIExtension -ext WixUtilExtension -ext WixFirewallExtension jenkins.wxs jre.wxs  || exit /b 1
 REM  '-sval' skips validation. without this, light somehow doesn't work on automated build environment
 REM  set to -dcl:low during debug and -dcl:high for release
-light -o %ARTIFACTNAME%.msi -sval -nologo -dcl:high -ext WixUIExtension -ext WixUtilExtension -ext WixFirewallExtension jenkins.wixobj jre.wixobj
+light -o %ARTIFACTNAME%.msi -sval -nologo -dcl:high -ext WixUIExtension -ext WixUtilExtension -ext WixFirewallExtension jenkins.wixobj jre.wixobj  || exit /b 1
 
-msbuild.exe /property:src=%ARTIFACTNAME%.msi "/property:ProductName=%PRODUCTNAME%" bootstrapper.xml
+msbuild.exe /property:src=%ARTIFACTNAME%.msi "/property:ProductName=%PRODUCTNAME%" bootstrapper.xml  || exit /b 1
 
-signtool sign /v /f key.pkcs12 /p $(cat key.password) /t http://timestamp.verisign.com/scripts/timestamp.dll %ARTIFACTNAME%.msi setup.exe
+signtool sign /v /f key.pkcs12 /p $(cat key.password) /t http://timestamp.verisign.com/scripts/timestamp.dll %ARTIFACTNAME%.msi setup.exe  || exit /b 1
 
 REM - the files we need to bring back are ${ARTIFACTNAME}.msi setup.exe
 REM zip ${ARTIFACTNAME}-windows.zip ${ARTIFACTNAME}.msi setup.exe
