46fee60df240c6509905db35e73285cd51af3b56
James Nord <jnord+github@cloudbees.com>
2015-09-25 16:39:25 +0100

    fix the version handling (hack)

--- osx/build-prep.sh
+++ osx/build-prep.sh
@@ -6,15 +6,20 @@ rm -fr $D
 mkdir -p $D/src
 
 cp -R $bin/* $D/src
-pushd $D/src/packages
-  pushd launchd_daemon/app
+pushd $D/src
+  pushd packages/launchd_daemon/app
     mv default.plist $OSX_IDPREFIX.plist
   popd
-  pushd launchd_user/app
+  pushd packages/launchd_user/app
     mv default.plist $OSX_IDPREFIX.plist
   popd
+# inline the version so that it is available.
+# TODO this should be done by templating (aka branding.)
+  mv build.sh build.sh_
+  sed s/@@VERSION@@/$VERSION/g < build.sh_ > build.sh
+  rm build.sh_
+  chmod +x build.sh
 popd
-echo $VERSION
 
 $BASE/bin/branding.sh $D/src
 
