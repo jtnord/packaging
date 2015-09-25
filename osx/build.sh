8daf6ffde8e5bf797f7ac1b927cbd11f02324b64
James Nord <jnord+github@cloudbees.com>
2015-09-25 18:18:19 +0100

    fix the version handling (hack)

--- osx/build.sh
+++ osx/build.sh
@@ -12,34 +12,34 @@ cp ~/jenkins.war packages/war/app/@@ARTIFACTNAME@@.war
 mkdir -p build/components
 
 pkgbuild --root packages/war/app \
-         --identifier @@OSX_IDPREFIX@@.war \
+         --identifier @@OSX_IDPREFIX@@.@@ARTIFACTNAME@@.war \
          --version @@VERSION@@ \
          --install-location /Applications/@@CAMELARTIFACTNAME@@ \
          --scripts packages/war/scripts \
          build/components/war.pkg
          
 pkgbuild --root packages/launchd_daemon/app \
-         --identifier @@OSX_IDPREFIX@@.launchd_daemon \
+         --identifier @@OSX_IDPREFIX@@.@@ARTIFACTNAME@@.launchd_daemon \
          --version @@VERSION@@ \
          --install-location /Library/LaunchDaemons/ \
          --scripts packages/launchd_daemon/scripts \
          build/components/launchd_daemon.pkg
 
 pkgbuild --root packages/launchd_user/app \
-         --identifier @@OSX_IDPREFIX@@.launchd_@@ARTIFACTNAME@@ \
+         --identifier @@OSX_IDPREFIX@@.@@ARTIFACTNAME@@.launchd_@@ARTIFACTNAME@@ \
          --version @@VERSION@@ \
          --install-location /Library/LaunchDaemons/ \
          --scripts packages/launchd_user/scripts \
          build/components/launchd_@@ARTIFACTNAME@@.pkg
 
 pkgbuild --root packages/support/app \
-         --identifier @@OSX_IDPREFIX@@.support \
+         --identifier @@OSX_IDPREFIX@@.@@ARTIFACTNAME@@.support \
          --version @@VERSION@@ \
          --install-location "/Library/Application Support/@@CAMELARTIFACTNAME@@" \
          build/components/support.pkg
 
 pkgbuild --root packages/documentation/app \
-         --identifier @@OSX_IDPREFIX@@.documentation \
+         --identifier @@OSX_IDPREFIX@@.@@ARTIFACTNAME@@.documentation \
          --version @@VERSION@@ \
          --install-location /Library/Documentation/@@CAMELARTIFACTNAME@@ \
          build/components/documentation.pkg
