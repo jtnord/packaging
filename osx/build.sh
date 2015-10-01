6bd1743ddacb6dcef6cbe89250d46cc5a58b9da0
James Nord <jnord+github@cloudbees.com>
2015-10-01 12:27:36 +0100

    it is PASSWORD you numbtee

--- osx/build.sh
+++ osx/build.sh
@@ -52,7 +52,7 @@ security create-keychain -p $PASSWORD `pwd`/installer.keychain
 # import the certificates into the keychain
 security import installer.pkcs12 -k `pwd`/installer.keychain -t agg -f pkcs12 -A -P $PASSWORD
 # unluck the keychain so we can use it
-security unlock-keychain -p $PASSED `pwd`/installer.keychain
+security unlock-keychain -p $PASSWORD `pwd`/installer.keychain
 # We need to know the identity of the certificate to use
 SIGN_IDENTITY="$(security find-identity $PWD/installer.keychain | grep "1)" | head -n1 | cut -f4 -d' ')"
 
