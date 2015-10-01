1784d415fc54845765272e8913d734228c0db181
James Nord <jnord+github@cloudbees.com>
2015-10-01 12:22:29 +0100

    use the correct location for the keys

--- osx/build-prep.sh
+++ osx/build-prep.sh
@@ -28,6 +28,6 @@ $BASE/bin/branding.sh $D/src
 mkdir -p $D/src/packages/war/app
 #cp ${WAR} $D/src/packages/war/app/${ARTIFACTNAME}.war
 
-cp ${PKCS12_FILE} $D/installer.pkcs12
+cp ${PKCS12_FILE} $D/src/installer.pkcs12
 #TODO - password should come from the environment. (injected via credentials when running in workflow)
-cp ${PKCS12_PASSWORD_FILE} $D/installer.password
+cp ${PKCS12_PASSWORD_FILE} $D/src/installer.password
