0006d15b3416070f08f8b8703f5e08ac7d000ad1
James Nord <jnord+github@cloudbees.com>
2016-01-12 12:32:47 +0000

    remove the fidge of double qupting for rsync.
    Tests show this should not be needed when setting environment variables
    and calling a bash script with a similar rsync command.
    
    THis points to some really bad interation between make and the shell.
    So lets park this for now and re-visit if/when we replace make with
    gradle.

--- deb/publish/publish.sh
+++ deb/publish/publish.sh
@@ -2,7 +2,7 @@
 bin="$(dirname $0)"
 
 ssh $PKGSERVER mkdir -p "'$DEBDIR/'"
-rsync -avz "${DEB}" "'$PKGSERVER:$DEBDIR/'"
+rsync -avz "${DEB}" "$PKGSERVER:$DEBDIR/"
 
 D=/tmp/$$
 mkdir -p $D/binary $D/contents
@@ -43,6 +43,6 @@ gpg --batch --no-use-agent --no-default-keyring --keyring "$GPG_KEYRING" --secre
 
 cp $D/binary/Packages.* $D/binary/Release $D/binary/Release.gpg $D/binary/Contents.gz $D/contents/binary
 
-rsync -avz $D/contents/ "$PKGSERVER:'$DEB_WEBDIR/'"
+rsync -avz $D/contents/ "$PKGSERVER:$DEB_WEBDIR/"
 
 rm -rf $D
